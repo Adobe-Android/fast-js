9150fd8e1117b9d6daba00d54e16632124314bf9
David Brown <tngamerboy@gmail.com>
2020-04-25 01:18:17 -0500

    Comment out unnecessary console.log statements

--- main.js
+++ main.js
@@ -1,20 +1,20 @@
 require('nan')
-console.log("finished \"nan\"");
+// console.log("finished \"nan\"");
 const { hello } = require('./build/Release/hello.node');
-console.log("finished \"hello\"");
+// console.log("finished \"hello\"");
 
-const cPlusPlusHelloTime = "c++ hello"
+const cPlusPlusHelloTime = "c++ call loop time"
 console.time(cPlusPlusHelloTime);
 hello();
 console.timeEnd(cPlusPlusHelloTime);
 
-const jsLoopTime = "js loop";
+const jsLoopTime = "js loop time";
 console.time(jsLoopTime);
 let count = 0;
-for (let i = 0; i < 1000000; i++) {
+for (let i = 0; i < 1000000000; i++) {
     count = i;
 }
-console.log("hello\n");
+console.log("hello from js");
 console.timeEnd(jsLoopTime);
 
-console.log("\"hello\" function called");
+// console.log("\"hello\" function called");
