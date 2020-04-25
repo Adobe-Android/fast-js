dffc1e8b961c756d5589eeae3b669040113d577b
David Brown <tngamerboy@gmail.com>
2020-04-25 01:17:11 -0500

    Play with measuring time directly from C++
    I added time measurement in C++ as well. The one remaining around the call to the native node module remains as that is the real cost due to the additional overhead from the node native module middleware layer.

--- src/hello.cc
+++ src/hello.cc
@@ -1,14 +1,26 @@
 #include <iostream>
+#include <chrono>
+#include <thread>
 #include <nan.h>
+
 using namespace v8;
+using namespace std::literals::chrono_literals;
 
 NAN_METHOD(hello) {
+  // Change the desired level of accuracy
+  std::cout.precision(10);
+  auto time_start = std::chrono::high_resolution_clock::now();
   int count = 0;
-  for (int i = 0; i < 1000000; i++) {
-    count = i;
+  for (int i = 0; i < 1'000'000'000; i++) {
+    count = i;    
   }
-
-  std::cout << "hello\n";
+  // slows things down to make it easier to measure
+  std::this_thread::sleep_for(std::chrono::milliseconds(10));
+  auto time_end = std::chrono::high_resolution_clock::now();
+  std::chrono::duration<double> duration = time_end - time_start;
+  std::cout << "hello from c++\n";
+  std::cout << "c++ loop time (milliseconds): " << std::chrono::duration_cast<std::chrono::milliseconds>(time_end - time_start).count() << "ms\n";
+  std::cout << "c++ loop time (nanoseconds): " << std::chrono::duration_cast<std::chrono::nanoseconds>(time_end - time_start).count() << "ns\n";
 }
 
 NAN_MODULE_INIT(init) {
